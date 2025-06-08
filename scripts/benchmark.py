f7f686d62b7701ed1eabe52b6927a55b8052ed86
Nikolay Falaleev <nikolasent@gmail.com>
2025-06-08 19:25:41 +0100

    chore: Enable all benchmarks

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 from src.video_io import VALIVideoReader, OpenCVVideoReader, \
-    AbstractVideoReader, TorchvisionVideoReader
+    AbstractVideoReader, TorchcodecVideoReader, TorchvisionVideoReader
 
 VIDEO_PATH = "/workdir/data/videos/test.mp4"
 N_PASSES = 3  # Number of times to repeat the benchmark for each video
@@ -15,6 +15,7 @@ FRAMES_TO_READ_SLICE = list(range(10, 200, 10))
 
 # Adjust the list of video readers to benchmark as needed
 VIDEO_READERS = [
+    TorchcodecVideoReader,
     OpenCVVideoReader,
     TorchvisionVideoReader,
     VALIVideoReader
--- src/video_io/__init__.py
+++ src/video_io/__init__.py
@@ -2,5 +2,5 @@ from .vali_reader import VALIVideoReader
 from .opencv_reader import OpenCVVideoReader
 from .abstract_reader import AbstractVideoReader
 # The current nightly version of TorchCodec does not work, at least in the env
-# from .torchcodec_reader import TorchcodecVideoReader
+from .torchcodec_reader import TorchcodecVideoReader
 from .torchvision_reader import TorchvisionVideoReader
