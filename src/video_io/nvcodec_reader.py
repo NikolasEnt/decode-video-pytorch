f179109cf9909ee5318699bdec97e0adf6c2b3c7
Nikolay Falaleev <nikolasent@gmail.com>
2025-06-10 02:56:41 +0100

    feat: Simplify PyNvVideoCodecReader

--- src/video_io/nvcodec_reader.py
+++ src/video_io/nvcodec_reader.py
@@ -15,11 +15,14 @@ class PyNvVideoCodecReader(AbstractVideoReader):
                  device: str = "cuda:0"):
         self.decoder = None
         super().__init__(video_path, mode, output_format, device)
+        if self.gpu_id < 0:
+            ValueError("PyNvVideoCodecReader supports Nvidia GPU decoding only"
+                       f"provide a valid device. {self.device} was specified.")
 
     def _initialize_reader(self) -> None:
         self.decoder = nvc.SimpleDecoder(
             enc_file_path=self.video_path,
-            gpu_id=int(self.device.split(':')[1]),
+            gpu_id=self.gpu_id,
             use_device_memory=True,
             output_color_type=nvc.OutputColorType.RGB
         )
@@ -31,27 +34,14 @@ class PyNvVideoCodecReader(AbstractVideoReader):
     def seek_read(self, frame_indices: list[int]) -> list[torch.Tensor]:
         frames = []
         for idx in frame_indices:
-            self.decoder.seek_to_index(idx)
             frames.append(torch.from_dlpack(self.decoder[idx]))
         return frames
 
     def stream_read(self, frame_indices: list[int]) -> list[torch.Tensor]:
-        # Using the range to read consecutive frames
-        start_idx = min(frame_indices)
-        self.decoder.seek_to_index(start_idx)
-        frames = self.decoder.get_batch_frames_by_index(frame_indices)
-        return [torch.from_dlpack(frame) for frame in frames]
+        return [torch.from_dlpack(frame) for frame in
+                self.decoder.get_batch_frames_by_index(frame_indices)]
 
     def release(self) -> None:
         if hasattr(self, 'decoder') and self.decoder is not None:
             del self.decoder
             self.decoder = None
-
-
-# Usage example
-if __name__ == "__main__":
-    reader = PyNvVideoCodecReader("/workdir/data/videos/test.mp4", mode="seek",
-                                  output_format="TCHW")
-    print(reader)
-    frames = reader.read_frames([0, 10, 20, 30])
-    print(frames.shape)
